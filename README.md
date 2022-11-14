ba40d425564dd2e7ce78ef79c7a6fabfb2ec1479
Hichamouazzani <98602974+Hichamouazzani@users.noreply.github.com>
2022-11-14 21:23:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltejs-kit-template-default-gx3rhb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-gx3rhb)
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this website</h1>
+
+	<p>
+		This is a food recipes website. You can make your own healthy and delicious dishes even if you have never cooked before! Just by following the simple instructions. 
+		
+	</p>
+</div>
+
+<style> 
+h1{
+	margin: 5px;
+	font-size: 50px;
+	font-family: Georgia, serif; 
+	color: red; 
+	text-align: center; 
+}
+p{
+	font-style: italic;
+	font-weight: bold; 
+}
+</style>
